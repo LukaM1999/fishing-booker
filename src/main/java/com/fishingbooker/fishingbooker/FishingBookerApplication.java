2869c4a77b9029c4b4eea3db09f32f474a030dbe
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2021-12-17 22:40:15 +0100

    initialized java spring boot backend

---
+++ src/main/java/com/fishingbooker/fishingbooker/FishingBookerApplication.java
@@ -0,0 +1,13 @@
+package com.fishingbooker.fishingbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingBookerApplication.class, args);
+	}
+
+}
